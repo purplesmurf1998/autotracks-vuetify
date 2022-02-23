693a6ba1ad29785148db99fc64c8eb6f4e016d53
purplesmurf1998 <cedrikdubois.edu@gmail.com>
2022-02-23 13:30:45 -0500

    Re-arranged files, added backend and docker

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Autotracks API listening on port ${port}`);
+})
